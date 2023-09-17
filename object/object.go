bca6b81d2b3c782995d03c586823bb91bcbed36a
towhid <ktowhid20@gmail.com>
2023-09-18 01:47:39 +0600

    implemented boolean in object

--- object/object.go
+++ object/object.go
@@ -10,11 +10,19 @@ type Object interface {
 
 const (
 	INTEGER_OBJ = "INTEGER"
-	)
+	BOOLEAN_OBJ = "BOOLEAN"
+)
 
 type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
