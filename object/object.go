ab268d6ae3a2bc05b11d5319f7e5884301005d4f
towhid <ktowhid20@gmail.com>
2023-09-18 01:36:19 +0600

    finished implementing integers in object

--- object/object.go
+++ object/object.go
@@ -1,7 +1,20 @@
 package object
 
+import "fmt"
+
 type ObjectType string
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	)
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
