54a0362162fb7211da2f822920e18018f1859830
towhid <ktowhid20@gmail.com>
2023-09-19 12:23:38 +0600

    started evaluation. Finished evaluating integer literal

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/nexentra/aesir/evaluator"
 	"github.com/nexentra/aesir/lexer"
 	"github.com/nexentra/aesir/parser"
 )
@@ -44,8 +45,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
