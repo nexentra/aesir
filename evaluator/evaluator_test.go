a5a671bfaaec9ec9a9c9dec02d627195ee7c7143
towhid <ktowhid20@gmail.com>
2023-09-19 15:38:23 +0600

    added evaluation for prefix operator(for negative operator)

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -16,12 +16,14 @@ func testEval(input string) object.Object {
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input    string
+		input string
 		expected int64
-	}{
+		}{
 		{"5", 5},
 		{"10", 10},
-	}
+		{"-5", -5},
+		{"-10", -10},
+		}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testIntegerObject(t, evaluated, tt.expected)
