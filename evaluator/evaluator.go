d5af0910d14d6569e2216df10c3d9229b51cad63
towhid <ktowhid20@gmail.com>
2023-09-25 16:06:39 +0600

    added support for string concat

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -166,6 +166,8 @@ func evalInfixExpression(
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s",
 			left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
@@ -325,4 +327,17 @@ func unwrapReturnValue(obj object.Object) object.Object {
 	}
 
 	return obj
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -207,14 +207,17 @@ func TestErrorHandling(t *testing.T) {
 		},
 		{
 			`
-	if (10 > 1) {
-	if (10 > 1) {
-	return true + false;
-	}
-	return 1;
-	}
-	`,
+			if (10 > 1) {
+				if (10 > 1) {
+					return true + false;
+				}
+					return 1;
+			}`,
 			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
 		},
 	}
 	for _, tt := range tests {
@@ -294,6 +297,18 @@ func TestClosures(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
 	if !ok {
