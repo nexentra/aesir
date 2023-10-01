b78b5030f2ac673f4f766dbfc181de02298f9357
towhid <ktowhid20@gmail.com>
2023-10-02 02:34:10 +0600

    added nextra for easily showing docs from mdx

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx'
+  })
+   
+  module.exports = withNextra()
---
+++ docs/theme.config.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    },
+    logo: <strong>Project</strong>
+  }
