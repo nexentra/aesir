105e7aa099fad447da912810b3cfb831c9fa261a
towhid <ktowhid20@gmail.com>
2023-10-24 16:08:47 +0600

    fixed repl not showing error correctly

--- aesir.go
+++ aesir.go
@@ -10,10 +10,10 @@ import (
 func Execute(input string, env *object.Environment) (string, []string) {
 	l := lexer.New(input)
 	p := parser.New(l)
+	program := p.ParseProgram()
 	if len(p.Errors()) != 0 {
 		return "", p.Errors()
 	}
-	program := p.ParseProgram()
 	evaluated := evaluator.Eval(program, env)
 	return evaluated.Inspect(), nil
 }
