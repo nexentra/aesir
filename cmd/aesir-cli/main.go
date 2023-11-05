c651245190191fcdc2d448fdc4ca259c27a85a25
towhid <ktowhid20@gmail.com>
2023-11-05 13:48:08 +0600

    implemented simple fs and multi-view

--- cmd/aesir-cli/main.go
+++ cmd/aesir-cli/main.go
@@ -1,15 +1,8 @@
 package main
 
-// An example demonstrating an application with multiple views.
-//
-// Note that this example was produced before the Bubbles progress component
-// was available (github.com/charmbracelet/bubbles/progress) and thus, we're
-// implementing a progress bar from scratch here.
-
 import (
 	"errors"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -17,267 +10,21 @@ import (
 
 	"github.com/charmbracelet/bubbles/filepicker"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/fogleman/ease"
-	"github.com/lucasb-eyer/go-colorful"
-	"github.com/muesli/reflow/indent"
 	"github.com/muesli/termenv"
 )
 
-const (
-	progressBarWidth  = 71
-	progressFullChar  = "█"
-	progressEmptyChar = "░"
-)
-
-// General stuff for styling the view
 var (
-	term          = termenv.EnvColorProfile()
-	keyword       = makeFgStyle("211")
-	subtle        = makeFgStyle("241")
-	progressEmpty = subtle(progressEmptyChar)
-	dot           = colorFg(" • ", "236")
-
-	// Gradient colors we'll use for the progress bar
-	ramp = makeRamp("#B14FFF", "#00FFA3", progressBarWidth)
+	term   = termenv.EnvColorProfile()
+	subtle = makeFgStyle("241")
+	dot    = colorFg(" • ", "236")
 )
 
-func main() {
-	var tm tea.Model
-	var err error
-	fp := filepicker.New()
-	fp.AllowedTypes = []string{".mod", ".sum", ".go", ".txt", ".md"}
-	fp.CurrentDirectory, _ = os.UserHomeDir()
-	// fp.ShowHidden = true
-
-	initialModel := model{fp, "", 0, false, 10, 0, 0, false, false}
-
-	p := tea.NewProgram(&initialModel, tea.WithOutput(os.Stderr))
-	if tm, err = p.Run(); err != nil {
-		fmt.Println("could not start program:", err)
-	}
-	mm := tm.(model)
-	fmt.Println("\n  You selected: " + initialModel.Filepicker.Styles.Selected.Render(mm.selectedFile) + "\n")
+func makeFgStyle(color string) func(string) string {
+	return termenv.Style{}.Foreground(term.Color(color)).Styled
 }
 
-type (
-	tickMsg  struct{}
-	frameMsg struct{}
-)
-
-func tick() tea.Cmd {
-	return tea.Tick(time.Second, func(time.Time) tea.Msg {
-		return tickMsg{}
-	})
-}
-
-func frame() tea.Cmd {
-	return tea.Tick(time.Second/60, func(time.Time) tea.Msg {
-		return frameMsg{}
-	})
-}
-
-type model struct {
-	Filepicker   filepicker.Model
-	selectedFile string
-	Choice       int
-	Chosen       bool
-	Ticks        int
-	Frames       int
-	Progress     float64
-	Loaded       bool
-	Quitting     bool
-}
-
-func (m model) Init() tea.Cmd {
-	return tea.Batch(tick(), m.Filepicker.Init())
-}
-
-// Main update function.
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	// Make sure these keys always quit
-	if msg, ok := msg.(tea.KeyMsg); ok {
-		k := msg.String()
-		if k == "q" || k == "esc" || k == "ctrl+c" {
-			m.Quitting = true
-			return m, tea.Quit
-		}
-	}
-	// var cmd tea.Cmd
-	m.Filepicker, _ = m.Filepicker.Update(msg)
-	// Did the user select a file?
-	// if didSelect, path := m.Filepicker.DidSelectFile(msg); didSelect {
-	// 	// Get the path of the selected file.
-	// 	m.selectedFile = path
-	// }
-
-	// // Did the user select a disabled file?
-	// // This is only necessary to display an error to the user.
-	// if didSelect, path := m.Filepicker.DidSelectDisabledFile(msg); didSelect {
-	// 	// Let's clear the selectedFile and display an error.
-	// 	fmt.Println(errors.New(path + " is not valid."))
-	// 	m.selectedFile = ""
-	// 	return m, tea.Batch(cmd, tick())
-	// }
-
-	// Hand off the message and model to the appropriate update function for the
-	// appropriate view based on the current state.
-	if !m.Chosen {
-		return updateChoices(msg, m)
-	}
-
-	// return m, tea.Batch(cmd, tick())
-	return updateChosen(msg, m)
-}
-
-// The main view, which just calls the appropriate sub-view
-func (m model) View() string {
-	var s string
-	if m.Quitting {
-		return "\n  See you later!\n\n"
-	}
-	if !m.Chosen {
-		s = choicesView(m)
-	} else {
-		s = chosenView(m)
-	}
-	return indent.String("\n"+s+"\n\n", 2)
-}
-
-// Sub-update functions
-
-// Update loop for the first view where you're choosing a task.
-func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "j", "down":
-			m.Choice++
-			if m.Choice > 3 {
-				m.Choice = 3
-			}
-		case "k", "up":
-			m.Choice--
-			if m.Choice < 0 {
-				m.Choice = 0
-			}
-		case "enter":
-			m.Chosen = true
-			return m, frame()
-		}
-
-	case tickMsg:
-		if m.Ticks == 0 {
-			m.Quitting = true
-			return m, tea.Quit
-		}
-		m.Ticks--
-		return m, tick()
-	}
-
-	if m.selectedFile == "" {
-		var cmd tea.Cmd
-		// m.Filepicker, cmd = m.Filepicker.Update(msg)
-
-		// Did the user select a file?
-		if didSelect, path := m.Filepicker.DidSelectFile(msg); didSelect {
-			// Get the path of the selected file.
-			m.selectedFile = path
-		}
-
-		// Did the user select a disabled file?
-		// This is only necessary to display an error to the user.
-		if didSelect, path := m.Filepicker.DidSelectDisabledFile(msg); didSelect {
-			// Let's clear the selectedFile and display an error.
-			fmt.Println(errors.New(path + " is not valid."))
-			m.selectedFile = ""
-			return m, tea.Batch(cmd, tick())
-		}
-
-		return m, cmd
-	}
-
-	return m, nil
-}
-
-// Update loop for the second view after a choice has been made
-func updateChosen(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
-	switch msg.(type) {
-	case frameMsg:
-		if !m.Loaded {
-			m.Frames++
-			m.Progress = ease.OutBounce(float64(m.Frames) / float64(100))
-			if m.Progress >= 1 {
-				m.Progress = 1
-				m.Loaded = true
-				m.Ticks = 100
-				return m, tick()
-			}
-			return m, frame()
-		}
-
-	case tickMsg:
-		if m.Loaded {
-			if m.Ticks == 0 {
-				m.Quitting = true
-				return m, tea.Quit
-			}
-			m.Ticks--
-			return m, tick()
-		}
-	}
-
-	return m, nil
-}
-
-// Sub-views
-
-// The first view, where you're choosing a task
-func choicesView(m model) string {
-	c := m.Choice
-
-	tpl := "What to do today?\n\n"
-	tpl += "%s\n\n"
-	tpl += "Program quits in %s seconds\n\n"
-	tpl += subtle("j/k, up/down: select") + dot + subtle("enter: choose") + dot + subtle("q, esc: quit")
-
-	choices := fmt.Sprintf(
-		"%s\n%s",
-		checkbox("Choose an Aesir file", c == 0),
-		checkbox("Start REPL", c == 1),
-	)
-
-	return fmt.Sprintf(tpl, choices, colorFg(strconv.Itoa(m.Ticks), "79"))
-}
-
-// The second view, after a task has been chosen
-func chosenView(m model) string {
-	var msg string
-
-	switch m.Choice {
-	case 0:
-		var s strings.Builder
-	s.WriteString("\n  ")
-	if m.selectedFile == "" {
-		s.WriteString("Pick a file:")
-	} else {
-		s.WriteString("Selected file: " + m.Filepicker.Styles.Selected.Render(m.selectedFile))
-	}
-	s.WriteString("\n\n" + m.Filepicker.View() + "\n")
-		msg = s.String()
-		// msg = fmt.Sprintf("Carrot planting?\n\nCool, we'll need %s and %s...", keyword("libgarden"), keyword("vegeutils"))
-	case 1:
-		msg = fmt.Sprintf("A trip to the market?\n\nOkay, then we should install %s and %s...", keyword("marketkit"), keyword("libshopping"))
-	default:
-		msg = fmt.Sprintf("It’s always good to see friends.\n\nFetching %s and %s...", keyword("social-skills"), keyword("conversationutils"))
-	}
-
-	label := "Downloading..."
-	if m.Loaded {
-		label = fmt.Sprintf("Downloaded. Exiting in %s seconds...", colorFg(strconv.Itoa(m.Ticks), "79"))
-	}
-
-	return msg + "\n\n" + label + "\n" + progressbar(m.Progress) + "%"
+func colorFg(val, color string) string {
+	return termenv.String(val).Foreground(term.Color(color)).String()
 }
 
 func checkbox(label string, checked bool) string {
@@ -287,56 +34,150 @@ func checkbox(label string, checked bool) string {
 	return fmt.Sprintf("[ ] %s", label)
 }
 
-func progressbar(percent float64) string {
-	w := float64(progressBarWidth)
+type Model struct {
+	page         string
+	filepicker   filepicker.Model
+	selectedFile string
+	Choice       int
+	Chosen       bool
+	quitting     bool
+	err          error
+}
 
-	fullSize := int(math.Round(w * percent))
-	var fullCells string
-	for i := 0; i < fullSize; i++ {
-		fullCells += termenv.String(progressFullChar).Foreground(term.Color(ramp[i])).String()
+type clearErrorMsg struct{}
+
+func clearErrorAfter(t time.Duration) tea.Cmd {
+	return tea.Tick(t, func(_ time.Time) tea.Msg {
+		return clearErrorMsg{}
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return 	m.filepicker.Init()
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+		case "enter":
+			// m.page = "fs"
+		}
+	case clearErrorMsg:
+		m.err = nil
 	}
 
-	emptySize := int(w) - fullSize
-	emptyCells := strings.Repeat(progressEmpty, emptySize)
+	var cmd tea.Cmd
+	m.filepicker, cmd = m.filepicker.Update(msg)
 
-	return fmt.Sprintf("%s%s %3.0f", fullCells, emptyCells, math.Round(percent*100))
-}
+	switch m.page {
+	case "fs":
+		if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
+			m.selectedFile = path
+		}
 
-// Utils
+		if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
+			m.err = errors.New(path + " is not valid.")
+			m.selectedFile = ""
+			return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
+		}
 
-// Color a string's foreground with the given value.
-func colorFg(val, color string) string {
-	return termenv.String(val).Foreground(term.Color(color)).String()
-}
+		return m, cmd
+	case "repl":
+		return m, tea.Quit
+	default:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "j", "down":
+				m.Choice++
+				if m.Choice > 1 {
+					m.Choice = 1
+				}
+			case "k", "up":
+				m.Choice--
+				if m.Choice < 0 {
+					m.Choice = 0
+				}
+			case "enter":
+				if m.Choice == 0 {
+					m.page = "fs"
+					// m.filepicker.CurrentDirectory = "./"
+				}
+				m.Chosen = true
+				return m, cmd
+			}
 
-// Return a function that will colorize the foreground of a given string.
-func makeFgStyle(color string) func(string) string {
-	return termenv.Style{}.Foreground(term.Color(color)).Styled
-}
-
-// Generate a blend of colors.
-func makeRamp(colorA, colorB string, steps float64) (s []string) {
-	cA, _ := colorful.Hex(colorA)
-	cB, _ := colorful.Hex(colorB)
-
-	for i := 0.0; i < steps; i++ {
-		c := cA.BlendLuv(cB, i/steps)
-		s = append(s, colorToHex(c))
+		}
 	}
-	return
-}
 
-// Convert a colorful.Color to a hexadecimal format compatible with termenv.
-func colorToHex(c colorful.Color) string {
-	return fmt.Sprintf("#%s%s%s", colorFloatToHex(c.R), colorFloatToHex(c.G), colorFloatToHex(c.B))
+	return m, nil
 }
-
-// Helper function for converting colors to hex. Assumes a value between 0 and
-// 1.
-func colorFloatToHex(f float64) (s string) {
-	s = strconv.FormatInt(int64(f*255), 16)
-	if len(s) == 1 {
-		s = "0" + s
+func (m Model) View() string {
+	if m.quitting {
+		return ""
 	}
-	return
+
+	switch m.page {
+	case "fs":
+		var s strings.Builder
+		s.WriteString("\n  ")
+		if m.err != nil {
+			s.WriteString(m.filepicker.Styles.DisabledFile.Render(m.err.Error()))
+		} else if m.selectedFile == "" {
+			s.WriteString("Pick a file:")
+		} else {
+			s.WriteString("Selected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile))
+		}
+		s.WriteString("\n\n" + m.filepicker.View() + "\n")
+		return s.String()
+	case "repl":
+		return "repl view"
+	default:
+
+		c := m.Choice
+
+		tpl := "What to do today?\n\n"
+		tpl += "%s\n\n"
+		tpl += "Program quits in %s seconds\n\n"
+		tpl += subtle("j/k, up/down: select") + dot + subtle("enter: choose") + dot + subtle("q, esc: quit")
+
+		choices := fmt.Sprintf(
+			"%s\n%s\n",
+			checkbox("Open File Picker", c == 0),
+			checkbox("Open REPL", c == 1),
+		)
+
+		return fmt.Sprintf(tpl, choices, colorFg(strconv.Itoa(m.Choice), "79"))
+
+	}
+}
+
+func main() {
+	fp := filepicker.New()
+	fp.AllowedTypes = []string{".ae"}
+	fp.CurrentDirectory, _ = os.UserHomeDir()
+
+	
+
+	m := Model{
+		filepicker: fp,
+	}
+
+	p := tea.NewProgram(
+		&m,
+		tea.WithOutput(os.Stdout),
+	)
+	var tm tea.Model
+	var err error
+	if tm, err = p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
+	mm := tm.(Model)
+	fmt.Println("\n  You selected: " + m.filepicker.Styles.Selected.Render(mm.selectedFile) + "\n")
 }
